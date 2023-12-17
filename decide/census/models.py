202307b83bd41559289350a56bae66919cd3a89d
santi <santizuleta11@gmail.com>
2023-12-17 13:40:37 +0100

    feat (Añade modelo CensusGroup y relación con Voting)

--- decide/census/models.py
+++ decide/census/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+from django.contrib.auth.models import User
 
+from django.core.exceptions import ValidationError
 
 class Census(models.Model):
     voting_id = models.PositiveIntegerField()
@@ -7,3 +10,18 @@ class Census(models.Model):
 
     class Meta:
         unique_together = (('voting_id', 'voter_id'),)
+
+
+class CensusGroup(models.Model):
+    name = models.CharField(max_length=200)
+    users = ArrayField(models.PositiveIntegerField(), default=list, blank=True)
+
+    def clean(self):
+        super().clean()
+
+        for user_id in self.users:
+            if not User.objects.filter(id=user_id).exists():
+                raise ValidationError(f"El usuario con ID {user_id} no existe")
+
+    def __str__(self):
+        return self.name
