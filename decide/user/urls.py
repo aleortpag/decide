42de77de022b689b1f6dfe3425d8e4d1d808fc42
jesvievaz <vvjesus32@gmail.com>
2023-12-19 20:51:46 +0100

    feat (Vista votaciones disponibles para usuario)

--- decide/user/urls.py
+++ decide/user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .views import AvailableVotingsView
+from booth.views import BoothView
 
 
 urlpatterns = [
-    path('<int:user_id>/', AvailableVotingsView.as_view()),
+    path('<int:user_id>/', AvailableVotingsView.as_view(), name="user"),
 ]
