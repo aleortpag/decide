254364e18ef0977582bf2d109241ac56e746a8a0
Alejandro Ortiz Pagador <48619510+aleortpag@users.noreply.github.com>
2023-12-19 22:07:51 +0100

    Update views.py

--- decide/user/views.py
+++ decide/user/views.py
@@ -1,11 +1,7 @@
-import json
 from django.views.generic import TemplateView
-from django.conf import settings
-from django.http import Http404
 from census.models import Census
 from voting.models import Voting
 
-from base import mods
 
 class AvailableVotingsView(TemplateView):
     template_name = 'user/availableVotings.html'
